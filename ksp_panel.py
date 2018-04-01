bd57d96f79a03a0964279b311753890d8a85130f
Snoo-py <37975133+Snoo-py@users.noreply.github.com>
2018-04-01 13:50:55 +0200

    Initial commit

---
+++ ksp_panel.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python3
+
+import sys
+import argparse
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal, pyqtSlot, QObject, QThread
+from PyQt5.QtGui import QIcon
+import numpy as np
+
+import time
+import krpc
+
+from mfd.orbital_mfd import OrbitalMFD
+
+
+
+class Interface(QMainWindow):
+    krpc_client_begin_connect = pyqtSignal()
+    ksp_connected = pyqtSignal()
+    ksp_disconnected = pyqtSignal()
+
+    def __init__(self, ksp_ip):
+        QMainWindow.__init__(self)
+        self.setWindowTitle("Kerbal nav")
+
+        self.orbital = OrbitalMFD(self, width=5, height=5)
+        self.orbital.move(0,0)
+        self.showMaximized()
+
+        self.ksp_thread = QThread()
+        self.ksp_client = KrpcClient(ksp_ip)
+        self.ksp_client.moveToThread(self.ksp_thread)
+        
+        self.krpc_client_begin_connect.connect(self.ksp_client.connect_to_ksp)
+        self.ksp_client.telemetry_updated.connect(self.telemetry_updated)
+
+        self.ksp_thread.start()
+
+        self.krpc_client_begin_connect.emit()
+
+
+    @pyqtSlot(dict)
+    def telemetry_updated(self, telemetry_data):
+        self.orbital.update_mfd(telemetry_data)
+
+
+
+class KrpcClient(QObject):
+    ksp_connected = pyqtSignal()
+    ksp_disconnected = pyqtSignal()
+    telemetry_updated = pyqtSignal(dict)
+
+    def __init__(self, server_address, **kwargs):
+        super(KrpcClient, self).__init__(**kwargs)
+
+        self.server_address = server_address
+
+        self.ksp_is_connected = False
+        self.ksp_current_game_scene = None
+        self.as_active_vessel = False
+        self.telemetry = {}
+
+         # initialize update timers
+        self._short_term_scheduler = QTimer()
+        self._short_term_scheduler.timeout.connect(self.short_term_processing)
+        self._short_term_scheduler.start(0.10 * 1000)
+
+
+    @pyqtSlot()
+    def connect_to_ksp(self):
+        try:
+            self.ksp_conn = krpc.connect(address=self.server_address)
+            self.ksp_is_connected = True
+            self.ksp_connected.emit()
+            print('Connection Success')
+        except OSError:
+            print('Connection error')
+            self.ksp_is_connected = False
+            return
+
+
+    @pyqtSlot()
+    def short_term_processing(self):
+        if self.ksp_is_connected:
+            # Check if the current game scene is a a Flight scene, continue processing
+            if self.ksp_conn.krpc.current_game_scene == self.ksp_conn.krpc.GameScene.flight:
+                # set up telemetry for the vessel if it hasn't already been done
+                if not self.as_active_vessel:
+                    self.init_telemetry()
+                else:
+                    self.update_telemetry()
+                    pass
+            else:
+                # otherwise, unset the active vessel and telemetry
+                self.as_active_vessel = False
+
+    
+    def init_telemetry(self):
+        self.space_center = self.ksp_conn.space_center
+        self.as_active_vessel = True
+        self.telemetry = {}
+
+
+    def update_telemetry(self):
+        orbit = self.space_center.active_vessel.orbit
+        #self.telemetry['active_vessel_id'] = self.current_vessel._object_id
+        self.telemetry['ut'] = self.space_center.ut
+        self.telemetry['apoapsis_altitude'] = orbit.apoapsis_altitude
+        self.telemetry['periapsis_altitude'] = orbit.periapsis_altitude
+        self.telemetry['eccentricity'] = orbit.eccentricity
+        self.telemetry['time_to_apoapsis'] = orbit.time_to_apoapsis
+        self.telemetry['time_to_periapsis'] = orbit.time_to_periapsis
+        self.telemetry['period'] = orbit.period
+        self.telemetry['inclination'] = orbit.inclination
+        self.telemetry['longitude_of_ascending_node'] = orbit.longitude_of_ascending_node
+        self.telemetry['argument_of_periapsis'] = orbit.argument_of_periapsis
+        self.telemetry['apoapsis'] = orbit.apoapsis
+        self.telemetry['periapsis'] = orbit.periapsis
+        self.telemetry['ref_body_name'] = orbit.body.name
+        self.telemetry['semi_major_axis'] = orbit.semi_major_axis
+        self.telemetry['semi_minor_axis'] = orbit.semi_minor_axis
+        self.telemetry['radius'] = orbit.radius
+        self.telemetry['orbital_speed'] = orbit.orbital_speed
+        self.telemetry['speed'] = orbit.speed
+        self.telemetry['true_anomaly'] = orbit.true_anomaly
+        
+        self.telemetry['time_to_ascending_node'] = orbit.ut_at_true_anomaly(-self.telemetry['argument_of_periapsis']) - self.telemetry['ut']
+        self.telemetry['time_to_descending_node'] = orbit.ut_at_true_anomaly(np.pi - self.telemetry['argument_of_periapsis']) - self.telemetry['ut']
+
+        self.telemetry['argument_of_periapsis_deg'] = np.rad2deg(orbit.argument_of_periapsis)
+        self.telemetry['longitude_of_ascending_node_deg'] = np.rad2deg(orbit.longitude_of_ascending_node)
+        self.telemetry['inclination_deg'] = np.rad2deg(orbit.inclination)
+        self.telemetry['true_anomaly_deg'] = np.rad2deg(orbit.true_anomaly)
+
+        self.telemetry_updated.emit(self.telemetry)
+ 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', '--host', help='ksp server')
+    args = parser.parse_args()
+
+    app = QApplication(sys.argv)
+    i = Interface(args.host)
+    sys.exit(app.exec_())
---
+++ mfd/ksp_mfd.py
@@ -0,0 +1,47 @@
+from PyQt5.QtWidgets import QSizePolicy
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+class KspMFD(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=5, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi, facecolor='black')
+
+        self.axes = fig.add_subplot(111)
+        fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+
+        self.axes.set_axis_off()
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+        self.ksp_disconnect = True
+
+    def update_mfd(self, telemetry):
+        """if not ksp_conn:
+            self.error_text("ERR: NO CONNECTION")
+        elif not ksp_conn.active_vessel:
+            self.error_text('ERR: NO DATA')
+        else:"""
+        self._update_mfd_data(telemetry)
+        self.draw()
+
+    def _update_mfd_data(self, telemetry):
+        pass
+
+    def disconnect(self):
+        self.ksp_disconnect = True
+
+    def connect(self):
+        self.ksp_disconnect = False
+
+    def error_text(self, text):
+        self.axes.text(0.5, 0.5, text,
+                       horizontalalignment='center',
+                       verticalalignment='center',
+                       transform=self.axes.transAxes,
+                       family='monospace',
+                       color='red')
